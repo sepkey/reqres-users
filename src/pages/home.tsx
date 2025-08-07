2935c259432e5df370349f9c69288b5cb07e7a18
Sepide Kia <sepidekia6990@gmail.com>
2025-08-07 19:29:23 +0330

    feat: design the home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,41 @@
 import { Typography } from "antd";
 
-const { Title } = Typography;
+const { Title, Paragraph } = Typography;
 
 export default function Home() {
   return (
-    <div>
-      <Title level={2}>Home</Title>
-      <p>Welcome to the dashboard!</p>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "70vh",
+        textAlign: "center",
+        gap: "24px",
+      }}
+    >
+      <img
+        src="./logo.svg"
+        alt="Logo"
+        style={{
+          width: "auto",
+          height: "120px",
+          maxWidth: "300px",
+          objectFit: "contain",
+        }}
+      />
+
+      <Title level={1}>Home</Title>
+
+      <Paragraph
+        style={{
+          fontSize: "18px",
+          color: "#8c8c8c",
+        }}
+      >
+        Welcome to the Sepide Kia's users dashboard!
+      </Paragraph>
     </div>
   );
 }
