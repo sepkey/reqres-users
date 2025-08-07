c2f63a50a440027d269239e85f562e952dc7a838
Sepide Kia <sepidekia6990@gmail.com>
2025-08-07 07:04:40 +0330

    fix:rename the variables

--- src/router/paths.ts
+++ src/router/paths.ts
@@ -1,6 +1,6 @@
 export const homePath = () => "/";
 export const usersPath = () => "/users";
-export const userPath = (ticketId: string) => `/users/${ticketId}`;
-export const userEditPath = (ticketId: string) => `/users/${ticketId}/edit`;
+export const userPath = (userId: string) => `/users/${userId}`;
+export const userEditPath = (userId: string) => `/users/${userId}/edit`;
 export const signUpPath = () => "/sign-up";
 export const signInPath = () => "/sign-in";
