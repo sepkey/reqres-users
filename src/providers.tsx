fbf21a657e4cf1b027aef06d6eb30f50f6014c80
Sepide Kia <sepidekia6990@gmail.com>
2025-08-07 06:38:16 +0330

    fix: change the color of active menu

--- src/providers.tsx
+++ src/providers.tsx
@@ -13,6 +13,15 @@ export default function Providers() {
           colorPrimary: "#08979c",
           borderRadius: 4,
         },
+        components: {
+          Menu: {
+            itemSelectedBg: "#f0f0f0",
+            itemSelectedColor: "#08979c",
+            itemHoverBg: "#f5f5f5",
+            itemHoverColor: "#08979c",
+            itemActiveBg: "#e6f7ff",
+          },
+        },
       }}
     >
       <QueryClientProvider client={queryClient}>
