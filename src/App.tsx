00d35d60434fb7205c4c56c6960ad7f0bcff16ba
Sepide Kia <sepidekia6990@gmail.com>
2025-08-08 08:27:02 +0330

    refactor: create a theme file and delete the providers instead

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-import Providers from "./providers";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ConfigProvider } from "antd";
+import { RouterProvider } from "react-router";
+import { router } from "./router";
+import { theme } from "./theme";
 
 export default function App() {
-  return <Providers />;
+  const queryClient = new QueryClient();
+
+  return (
+    <ConfigProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </ConfigProvider>
+  );
 }
